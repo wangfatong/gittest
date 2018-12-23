f4223fea988863931604731aaa9654e66ca5b186
wangfatong <1032215875@qq.com>
2018-12-23 20:41:24 +0800

    提交修改

--- fatongtest/src/main/java/com/fatong/domain/GitTest.java
+++ fatongtest/src/main/java/com/fatong/domain/GitTest.java
@@ -3,5 +3,6 @@ package com.fatong.domain;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("您好，这里是GIT!");
+        System.out.println("您好，世界！");
     }
 }
