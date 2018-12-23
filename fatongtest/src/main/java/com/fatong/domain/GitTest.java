4f5a8105f42233d37a9fa612297643b263363baf
wangfatong <1032215875@qq.com>
2018-12-23 21:19:09 +0800

    新加了类，并准备将其提交到远程仓库

---
+++ fatongtest/src/main/java/com/fatong/domain/GitTest.java
@@ -0,0 +1,8 @@
+package com.fatong.domain;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("世界，您好！");
+        System.out.println("我要将您提交到远程仓库！");
+    }
+}
