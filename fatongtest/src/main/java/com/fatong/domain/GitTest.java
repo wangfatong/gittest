327bdd93919bddfd8cbad2bf50c9471ff74642fa
wangfatong <1032215875@qq.com>
2018-12-23 20:43:50 +0800

    提交删除

--- fatongtest/src/main/java/com/fatong/domain/GitTest.java
+++
@@ -1,8 +0,0 @@
-package com.fatong.domain;
-
-public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("您好，这里是GIT!");
-        System.out.println("您好，世界！");
-    }
-}
